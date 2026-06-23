34af7c4de4e8153ca79081ed61e585bb42570bee
MrVHappy <sebcomyns@gmail.com>
2026-06-23 11:27:40 +0100

    allowed for cleanup and fixed issues

--- src/SondFont.cpp
+++ src/SondFont.cpp
@@ -12,21 +12,18 @@ class SoundFont{
         // map where it stores the instrument number and a bool that
         // indicates if the instrument is supported
         std::map<int,bool> instruments;
-
-        // attributes used for sound font analysis 
-        fluid_synth_t* synth = NULL;
-        fluid_settings_t* settings = NULL;
-        int sfont_ID;
-        fluid_sfont_t* sfont = NULL;
+        
 
     // constructor
     SoundFont(char* file){
         this->file = file;
-        // Create settings
-        settings = new_fluid_settings();
-        synth = new_fluid_synth(settings);
-        sfont_ID = fluid_synth_sfload(synth,this->file, 1);
-        sfont = fluid_synth_get_sfont_by_id(synth,sfont_ID);
+        
+        // variables used for sound font analysis 
+        fluid_settings_t* settings = new_fluid_settings();
+        fluid_synth_t* synth = new_fluid_synth(settings);
+        int sfont_ID = fluid_synth_sfload(synth,this->file, 1);
+        fluid_sfont_t* sfont = fluid_synth_get_sfont_by_id(synth,sfont_ID);
+        
         // setup for instruments
         // loops through all the instruments
         for (int i = 0; i < 127; i++){
@@ -41,8 +38,8 @@ class SoundFont{
             }
             // add instrument data to map
             instruments[i] = isSupported;
-            
         }
+        clean_up(settings,synth,sfont);
     }
 
     
@@ -54,7 +51,7 @@ class SoundFont{
         // checks if the sound font supports the instrument
         bool verify_instrument(int instrument_num){
             // returns false if instrument_num is out of range
-            if(instrument_num < 0 || instrument_num >127){
+            if(instrument_num < 0 || instrument_num >126){
                 return false;
             }
             return instruments[instrument_num];
@@ -66,7 +63,7 @@ class SoundFont{
             int category_index = 0;
             for (int i = 0; i < 128; i++){
                 // displays the next category
-                if (category_index % 8 == 0){
+                if (i % 8 == 0){
                     std::cout << instrument_category[category_index] << std::endl;
                     category_index++;
                 }
@@ -74,6 +71,17 @@ class SoundFont{
                 if(instruments[i] == true){
                     std::cout << i << ":\t" << smf::MidiFile::getGMInstrumentName(i)<<std::endl;
                 }
+            }
+        }
+        void clean_up(fluid_settings_t* settings,fluid_synth_t*, fluid_sfont_t* sfont){
+            if (synth != NULL) {
+                delete_fluid_synth(synth);
+            }
+            if (settings != NULL) {
+                delete_fluid_settings(settings);
+            }
+            if (sfont != NULL){
+                delete_fluid_sfont(sfont);
             }
         }
 };
